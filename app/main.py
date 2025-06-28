c670b60ebcccb58f1983c485cece7e009107324f
ZEUS-IT <daniel.masuco.zeus@gmail.com>
2025-06-28 16:09:23 -0300

    actualizacion main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.auth import router as auth_router
 from app.routers import polizas
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,11 @@ app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(polizas.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Origen del frontend local
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
